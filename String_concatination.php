090ec102ec2a5242cf746b352c883d3ecb037238
RITIK RAJ SINGH <128345776+RitikRajSingh2522000@users.noreply.github.com>
2024-01-15 12:26:39 +0530

    Update String_concatination.php
    added strlen() function here

--- String_concatination.php
+++ String_concatination.php
@@ -9,4 +9,12 @@ $name="Ritik";
 $name .= "Raj";
 echo $name;
 
+// Q) How to find the length of words or sentence in PHP?
+// PHP strlen() function => The strlen() function returns the length of a string.
+$data= "Hello, Good Morning this is 'Ritik'. ";
+$length = strlen($data);
+echo "The length of the string is". $length;// concating the variable with message. 
+?>
+
+
 ?>
