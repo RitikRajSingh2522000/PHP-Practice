ef767f6086d3f9d37be05180ea3428fe74455d66
RITIK RAJ SINGH <128345776+RitikRajSingh2522000@users.noreply.github.com>
2024-01-15 11:24:29 +0530

    Update == vs === test.php
    Added a difference between == and === in PHP

--- test.php
+++ test.php
@@ -11,4 +11,31 @@ echo $foo;                 // $foo is altered too.
 echo "If you view the source of output frame \r\n you will find a newline in this string.";
 echo "<br>";
 echo nl2br("You will find the \n newlines in this string \r\n on the browser window.");
+
+
+
+
+/*
+            PHP == vs === difference
+
+==      equal           $x==$y      Return true if $x is equal to $y.
+===     identical       $x===$y     Return true if $x is equal to $y, and they are ofthe same type.
+
+*/
+$num1=5;
+$num2="5";
+if($num1 == $num2){ // 
+    echo "both the value are same</br>";
+}else{
+    echo "values are same but data types are different</br>";
+}
+
+
+if($num1 === $num2){ // 
+    echo "both the value are same</br>";
+}else{
+    echo "values are same but data types are different</br?";
+}
+
+
 ?>
